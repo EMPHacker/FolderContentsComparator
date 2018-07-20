7b687a8e28d620b575ae471088105a318f7d2192
Tristan Chutka <tristan.chutka@outlook.com>
2018-07-20 01:03:51 -0700

    initial files

--- FileCompare/MainWindow.xaml.cs
+++ FileCompare/MainWindow.xaml.cs
@@ -1,19 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.Win32;
 using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace FileCompare
@@ -23,13 +14,26 @@ namespace FileCompare
     /// </summary>
     public partial class MainWindow : Window
     {
-        public struct FolderStructure
+        public class FolderStructure
         {
             public string Name;
             public string FolderPath;
-            public List<string> Files;
+            public bool Duplicate;
+            public List<FileItem> Files;
             public List<FolderStructure> Subdirectories;
-                
+
+            
+            public FolderStructure()
+            {
+                Duplicate = false;
+                Files = new List<FileItem>();
+                Subdirectories = new List<FolderStructure>();
+            }
+        }
+        public class FileItem
+        {
+            public string FileName;
+            public bool Duplicate;            
         }
 
         FolderStructure Folder1;
@@ -37,14 +41,11 @@ namespace FileCompare
 
         public MainWindow()
         {
-            
-
             InitializeComponent();
         }
 
         private void BrowseFolder1_Click(object sender, RoutedEventArgs e)
         {
-
             CommonOpenFileDialog dialog = new CommonOpenFileDialog
             {
                 IsFolderPicker = true
@@ -57,7 +58,7 @@ namespace FileCompare
                 {
                     Name = dialog.FileName.Split('\\').Last(),
                     FolderPath = dialog.FileName
-                };
+            };
             }
         }
 
@@ -78,81 +79,159 @@ namespace FileCompare
             }
         }
 
-
-        private static void ProcessSubdir(FolderStructure ParentDir, string targetdir)
-        {
-            ParentDir.Subdirectories = new List<FolderStructure>();
-
-            // Process the list of files found in the directory.
-            //NewSubFolder.Files = Directory.GetFiles(targetdir).ToList();
-
-            List<string> processedfiles = new List<string>();
-            string[] fileslist = Directory.GetFiles(targetdir);
-            foreach (string filename in fileslist)
-            {
-                processedfiles.Add(filename.Split('\\').Last());
-            }
-
-            FolderStructure NewSubFolder = new FolderStructure
-            {
-                Name = targetdir.Split('\\').Last(),
-                FolderPath = targetdir,
-                Files = processedfiles
-
-            };
-
-            // Recurse into subdirectories of this directory.
-            string[] subdirslist = Directory.GetDirectories(targetdir);
-            foreach (string subdir in subdirslist)
-            {
-                ProcessSubdir(NewSubFolder, subdir);
-            }
-
-
-            ParentDir.Subdirectories.Add(NewSubFolder);
-
-        }
-
         private void CompareProcessBtn_Click(object sender, RoutedEventArgs e)
         {
-            VisualFeedbackBar.Visibility = Visibility.Visible;
-
-            Task GetFiles = Task.Factory.StartNew(() => ProcessSubdir(Folder1, Folder1.FolderPath));
-
-            //Task.Run(async() => await ProcessSubdir(Folder1, Folder1.FolderPath));
-            //await ProcessSubdir(Folder1, Folder1.FolderPath);
-
-            GetFiles.Wait();
-            
-            //AddColsFolder1(Folder1, 0);
-            foreach (string file in Folder1.Subdirectories[0].Files)
+            if ((URLFolder1.Text != "") && (URLFolder1.Text != ""))
             {
-                FolderContents2.Items.Add(new ListViewItem { Content = file });
+                VisualFeedbackBar.Visibility = Visibility.Visible;
+
+                Task GetFiles = Task.Factory.StartNew(() =>
+                {
+                    ProcessSubdirectory(Folder1, Folder1.FolderPath);
+                    ProcessSubdirectory(Folder2, Folder2.FolderPath);
+                });
+
+                GetFiles.Wait();
+
+                if (GetFiles.IsCompleted)
+                {
+                    TreeViewItem RootNode1 = new TreeViewItem
+                    {
+                        Header = Folder1.Name,
+                        FontWeight = FontWeights.Normal
+                    };
+                    TreeViewItem RootNode2 = new TreeViewItem
+                    {
+                        Header = Folder2.Name,
+                        FontWeight = FontWeights.Normal
+                    };
+
+                    Task SortFiles = Task.Factory.StartNew(() =>
+                    {
+                        SortFolders(Folder1);
+                        SortFolders(Folder2);
+                    });
+                    SortFiles.Wait();
+
+                    if (SortFiles.IsCompleted)
+                    {
+                        CompareFolders(Folder1, Folder2);
+
+                        GenerateTreeNodes(Folder1, RootNode1);
+                        GenerateTreeNodes(Folder2, RootNode2);
+
+                        ContentsFolder1.Items.Add(RootNode1);
+                        ContentsFolder2.Items.Add(RootNode2);
+
+                        VisualFeedbackBar.Visibility = Visibility.Hidden;
+                    }
+                }
             }
-
-            ContentsFolder1.Items.Add(new ListViewItem());
-
+            else
+            {
+                MessageBox.Show(this, "Select two folders");
+            }
         }
 
-        private void AddColsFolder1(FolderStructure folder, int counter)
+        private static void ProcessSubdirectory(FolderStructure thisDirectory, string directoryPath)
         {
-            DataGridTextColumn newcol = new DataGridTextColumn
+            // Process the list of files found in the directory.
+            string[] fileslist = Directory.GetFiles(directoryPath);
+            foreach (string filename in fileslist)
             {
-                Width = 10,
-                Binding = new Binding(counter.ToString())
-            };
-            ContentsFolder1.Columns.Add(newcol);
-
-            foreach (string file in folder.Files)
-            {
-                ContentsFolder1.Items.Add(new ListViewItem());
-
+                FileItem newfile = new FileItem
+                {
+                    FileName = filename.Split('\\').Last(),
+                    Duplicate = false
+                };
+                thisDirectory.Files.Add(newfile);
             }
 
+            // Recurse into subdirectories of this directory.
+            string[] subdirslist = Directory.GetDirectories(directoryPath);
+            foreach (string subdir in subdirslist)
+            {
+                FolderStructure NewSubfolder = new FolderStructure
+                {
+                    Name = subdir.Split('\\').Last(),
+                    FolderPath = subdir
+                };
+                thisDirectory.Subdirectories.Add(NewSubfolder);
+
+                ProcessSubdirectory(NewSubfolder, subdir);
+            }
+        }
+
+        public void SortFolders(FolderStructure folder)
+        {
+            //folder.Files.Sort(x => x.FileName); var sortedEnumerable = 
+            folder.Files.OrderBy(p => p.FileName);
 
             foreach (FolderStructure subdir in folder.Subdirectories)
             {
-                AddColsFolder1(subdir, counter++);                    
+                SortFolders(subdir);
+            }
+        }
+
+        private void CompareFolders(FolderStructure firstFolder, FolderStructure secondFolder)
+        {
+            int fffcount = firstFolder.Files.Count();
+            int sffcount = secondFolder.Files.Count();
+
+            if ( fffcount == sffcount)
+            {
+                for (int i = 0; i < fffcount; i++)
+                {
+                    if (firstFolder.Files[i].FileName == secondFolder.Files[i].FileName)
+                    {
+                        firstFolder.Files[i].Duplicate = true;
+                        secondFolder.Files[i].Duplicate = true;
+                    }                    
+                }
+            }
+
+            int ffsfcount = firstFolder.Subdirectories.Count();
+            int sfsfcount = secondFolder.Subdirectories.Count();
+
+            for (int i = 0; i < ffsfcount; i++)
+            {
+                if (firstFolder.Subdirectories[i].Name == secondFolder.Subdirectories[i].Name)
+                {
+                    firstFolder.Subdirectories[i].Duplicate = true;
+                    secondFolder.Subdirectories[i].Duplicate = true;
+                    CompareFolders(firstFolder.Subdirectories[i], secondFolder.Subdirectories[i]);
+                }
+            }
+        }
+
+        private void GenerateTreeNodes(FolderStructure Folder, TreeViewItem rootNode)
+        {
+            foreach (FileItem file in Folder.Files)
+            {
+                TreeViewItem newnode = new TreeViewItem
+                {
+                    Header = file.FileName,
+                    FontWeight = FontWeights.Normal
+                };
+                if (file.Duplicate)
+                {
+                    newnode.Foreground = new SolidColorBrush(Colors.Red);
+                }
+                rootNode.Items.Add(newnode);
+            }
+            foreach (FolderStructure subdir in Folder.Subdirectories)
+            {
+                TreeViewItem newnode = new TreeViewItem
+                {
+                    Header = subdir.Name,
+                    FontWeight = FontWeights.Normal
+                };
+                if (subdir.Duplicate)
+                {
+                    newnode.Foreground = new SolidColorBrush(Colors.Red);
+                }
+                rootNode.Items.Add(newnode);
+                GenerateTreeNodes(subdir, newnode);
             }
         }
     }
